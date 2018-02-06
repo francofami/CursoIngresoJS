817645cc9ab58805b78ec7e9f550caf8787958ef
francofami <31368175+francofami@users.noreply.github.com>
2018-02-06 19:41:09 -0300

    Ejercicio 1
    parseFloat

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,37 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var precioUno;
+	var precioDos;
+	var precioTres;
+
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+
+	sumaproductos = parseFloat(precioUno) + parseFloat(precioDos) + parseFloat(precioTres);
+	alert("La suma de los precios es de: "+sumaproductos);
+
 }
 function Promedio () 
 {
-	
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+
+	sumaproductos = parseFloat(precioUno) + parseFloat(precioDos) + parseFloat(precioTres);
+	promedio = parseFloat(sumaproductos) / 3;
+	alert("El promedio es: "+promedio)
 }
 function PrecioFinal () 
 {
-	
+	precioUno=document.getElementById('PrecioUno').value;
+	precioDos=document.getElementById('PrecioDos').value;
+	precioTres=document.getElementById('PrecioTres').value;
+
+	sumaproductos = parseFloat(precioUno) + parseFloat(precioDos) + parseFloat(precioTres);
+    iva = (parseFloat(sumaproductos) * 21) / 100;
+    preciofinal = parseFloat(sumaproductos) + parseFloat(iva);
+    alert("El precio final (más IVA 21%) es de: "+preciofinal);
+
 }
