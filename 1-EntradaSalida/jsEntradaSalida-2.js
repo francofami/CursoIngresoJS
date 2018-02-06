b7ddc70a57bfdddaf77666359a3e9d15cfa5ba51
francofami <31368175+francofami@users.noreply.github.com>
2018-02-06 10:15:31 -0300

    Add files via upload
    alert, prompt, concatenar, comentar, variables

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,20 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+	var nombre;
+	nombre=prompt("Ingrese nombre","natalia natalia"); 
+	//Las primeras "" tiran un texto en pantalla y las segundas hacen que aparezca un valor por defecto en el cuadro de ingreso de texto
+	alert("Su nombre es: "+nombre); //El + es para concatenar, seria un equivalente al << en C
 	
+	//Esto es un comentario de una linea
+    
+	/*
+	esto es un bloque
+	alert("nombre");
+	nombre="Franco";
+	alert(nombre);
+    */
+
+	//var person = prompt("Please enter your name", "Harry Potter");
 }
 
