9150b5c84e71f6c8021d8d393e64078576526172
francofami <31368175+francofami@users.noreply.github.com>
2018-02-07 11:48:10 -0300

    1, 2, 3, 4 y 5
    If

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,18 @@
 function Mostrar()
 {
 //tomo la edad  
+	var edad;
 
+	edad= document .getElementById('edad').value;
+
+	if(edad>=18)
+	{
+		alert("Sos mayor de edad");
+	}
+
+	if(edad<=17)
+	{
+		alert("Sos menor de edad");
+	}
 
 }//FIN DE LA FUNCIÓN
