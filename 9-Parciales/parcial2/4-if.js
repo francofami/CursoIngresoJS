9670203b1292495b62de79edb4d70e3fa13c2414
francofami <31368175+francofami@users.noreply.github.com>
2018-02-09 00:45:15 -0300

    4

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,35 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
-	
+	var numeroUno;
+	var numeroDos;
+	var multiplicar;
+	var resta;
+	var suma;
+
+	numeroUno=prompt("Ingrese un numero:");
+	numeroDos=prompt("Ingrese otro numero:");
+
+	numeroUno=parseFloat(numeroUno);
+	numeroDos=parseFloat(numeroDos);
+
+
+	if(numeroUno==numeroDos)
+	{
+		multiplicar=numeroUno*numeroDos;
+		document.write("El producto entre los numeros es igual a : "+multiplicar);
+	}
+else
+	if(numeroUno>numeroDos)
+	{
+		resta=numeroUno-numeroDos;
+		document.write("La resta entre los numeros es igual a : "+resta);
+	}
+else
+	{
+		suma=numeroUno+numeroDos;
+		document.write("La suma entre los numeros es igual a : "+suma);		
+	}
+
 }
 
