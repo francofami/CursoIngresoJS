964aa1221971cf87ee4bf397eeec46e93ce2f10e
francofami <31368175+francofami@users.noreply.github.com>
2018-02-08 23:51:58 -0300

    4
    document.write

--- 9-Parciales/recuperatorio/cuatro.js
+++ 9-Parciales/recuperatorio/cuatro.js
@@ -1,4 +1,26 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var suma;
 
+	numeroUno=prompt("Ingrese un numero:");
+	numeroDos=prompt("Ingrese otro numero:");
+
+	numeroUno=parseFloat(numeroUno);
+	numeroDos=parseFloat(numeroDos);
+
+	suma=numeroUno+numeroDos;
+
+	if(suma>0)
+	{
+		document.write("El resultado es positivo");
+	}
+else
+	if(suma<0)
+	{
+		document.write("El resultado es negativo");
+	}
+else
+	document.write("El resultado es igual a 0");
 }
