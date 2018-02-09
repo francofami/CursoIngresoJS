10d57bd0b709a3603135c10446235e79f3038099
francofami <31368175+francofami@users.noreply.github.com>
2018-02-08 23:45:06 -0300

    3

--- 9-Parciales/recuperatorio/tres.js
+++ 9-Parciales/recuperatorio/tres.js
@@ -1,4 +1,23 @@
 function Mostrar()
 {
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var suma;
+	var promedio;
 
+	precioUno=document.getElementById('precioUno').value;
+	precioDos=document.getElementById('precioDos').value;
+	precioTres=document.getElementById('precioTres').value;
+
+	precioUno=parseFloat(precioUno);
+	precioDos=parseFloat(precioDos);
+	precioTres=parseFloat(precioTres);
+
+	suma=precioUno+precioDos+precioTres;
+
+	promedio=suma/3;
+
+	alert("La suma de los precios es: "+suma);
+	alert("El promedio de los precios es: "+promedio);
 }
