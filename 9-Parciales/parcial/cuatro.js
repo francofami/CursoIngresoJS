163f5bf573e0c9afae97ba95a5e564d5243718c6
francofami <31368175+francofami@users.noreply.github.com>
2018-02-08 22:52:56 -0300

    4
    sacar texto en pantalla por document.write

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,32 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var multiplicacion;
+	var resta;
 
+	numeroUno = prompt("Ingrese un numero: ");
+	numeroDos = prompt("Ingrese otro numero: ");
+
+	numeroUno=parseFloat(numeroUno);
+	numeroDos=parseFloat(numeroDos);
+
+	/* Es necesario pasar los numeros a parseFloat/Int antes de arrancar con los if y no despues del ultimo else
+	porque de esa manera no me funcíonó. */
+
+	if(numeroUno==numeroDos)
+	{
+		multiplicacion=numeroUno*numeroDos;
+		document.write(multiplicacion);
+	}
+else
+	if(numeroUno>numeroDos)
+	{
+		resta = numeroUno-numeroDos;
+		document.write(resta);
+	}
+else
+	
+	suma=numeroUno+numeroDos;
+	document.write(suma);
 }
