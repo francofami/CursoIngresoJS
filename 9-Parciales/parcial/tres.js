cb5320da8ee96420316f567384b8cc7dd7ded711
francofami <31368175+francofami@users.noreply.github.com>
2018-02-08 22:28:07 -0300

    3

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,22 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var alambreLargo;
+	var alambreAncho;
+	var alambrePerimetro;
+
+	largo=document.getElementById('alrgo').value;
+	ancho=document.getElementById('ancho').value;
+
+	alambreLargo = largo*6;
+	alambreAncho = ancho*6;
+
+	alambreLargo = parseFloat(alambreLargo);
+	alambreAncho = parseFloat(alambreAncho);
+
+	alambrePerimetro = alambreLargo+alambreAncho;
+
+	alert("Se necesitarán "+alambrePerimetro+" metros de alambre.");
 
 }
