57ed9fafb709b026b4e066c233da817e8da09e43
francofami <31368175+francofami@users.noreply.github.com>
2018-02-09 00:21:11 -0300

    3

--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -1,7 +1,23 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 3-EntradaSalida");
-	
+	var largo;
+	var ancho;
+	var perimetro;
+	var alambre;
+
+	largo=document.getElementById('largo').value;
+	ancho=document.getElementById('ancho').value;
+
+	largo=parseFloat(largo);
+	ancho=parseFloat(ancho);
+
+	perimetro=largo*2+ancho*2;
+
+	alambre=perimetro*6;
+
+	alert("Se necesitaran: "+alambre+" metros de alambre.");
+
+
 }
 
