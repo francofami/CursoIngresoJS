5f0015f720ceb14c6fb26d5bace5c46063ded575
francofami <31368175+francofami@users.noreply.github.com>
2018-02-09 00:12:58 -0300

    2

--- 9-Parciales/parcial2/2-entradaSalida.js
+++ 9-Parciales/parcial2/2-entradaSalida.js
@@ -1,7 +1,14 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 2-EntradaSalida");
-	
+	var importe;
+	var importeFinal;
+
+	importe=prompt("Ingrese importe:");
+
+	importeFinal=importe*1.21;
+
+	document.getElementById('importe').value=importeFinal;
+
 }
 
