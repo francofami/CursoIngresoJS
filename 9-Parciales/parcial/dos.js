2cae8da50d54247d9241a5fd2d73a32c806f824e
francofami <31368175+francofami@users.noreply.github.com>
2018-02-08 21:38:33 -0300

    2
    sumarle iva a un importe y mostrarlo por id

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,21 @@
 function Mostrar()
 {
-  
+  var importe;
+  var iva;
+
+  importe = prompt("Ingrese el importe");
+
+  importeFinal=importe*1.21; 
+
+  /* El importe*1.21 sale de: 
+
+  importe * 21/100 + importe --> eso me va a dar el importe final
+  importe * 0.21 + importe --> simplifico el 21 con el 100
+  importe*(0.21+1) --> saco factor común importe
+
+ */
+
+  document.getElementById('importeFinal').value = importeFinal;
+
+
 }
