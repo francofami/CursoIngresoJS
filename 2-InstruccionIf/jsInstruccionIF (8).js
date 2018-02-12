ccddc0f22e0078ae2a93c4ba19c169c05016de4a
francofami <31368175+francofami@users.noreply.github.com>
2018-02-12 20:22:17 -0300

    8

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,17 @@ function Mostrar()
 {
 //tomo la edad  
 
-	
+	var edad;
+	var estadoCivil;
+
+	edad=document.getElementById('edad').value;
+	estadoCivil=document.getElementById('estadoCivil').value;
+
+
+	if(edad>=18 && estadoCivil=="Soltero")
+	{
+		alert("Es soltero y no es menor");
+	}
 
 
 }//FIN DE LA FUNCIÓN
