a9788b7b73e73732dfbc886e55f8f320a2834acb
francofami <31368175+francofami@users.noreply.github.com>
2018-02-12 20:41:40 -0300

    10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,20 @@ function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var nota;
+
+	nota=Math.floor((Math.random()*10)+1);
+
+	if(nota==9 || nota==10)
+	{
+		alert("EXCELENTE");
+	}
+else
+	if(nota>=4 && nota<9)
+	{
+		alert("APROBÓ");
+	}
+else
+	alert("Vamos, la proxima se puede");
 
 }//FIN DE LA FUNCIÓN
