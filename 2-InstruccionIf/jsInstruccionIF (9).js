9311a2f0c6ee4f602e733834affdbc7741afcb9a
francofami <31368175+francofami@users.noreply.github.com>
2018-02-12 20:35:48 -0300

    9

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,15 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+
+	var numeroRandom;
+
+	numeroRandom=Math.floor((Math.random()*10)+1); 
+
+	/*	numeroRandom=Math.floor(Math.random()*10) hace del 0 al 9
+	  	numeroRandom=Math.floor((Math.random()*10)+1) hace del 1 al 10
+	*/
+
+	alert(numeroRandom);
 
 }//FIN DE LA FUNCIÓN
