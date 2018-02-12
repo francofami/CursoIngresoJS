7c88d13c3a1b679db94450f5218f32fb68340397
francofami <31368175+francofami@users.noreply.github.com>
2018-02-12 20:04:29 -0300

    1, 2, 3 y 4

--- 9-Parciales/recuperatorio2/3-entradaSalida.js
+++ 9-Parciales/recuperatorio2/3-entradaSalida.js
@@ -1,7 +1,25 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 3-EntradaSalida");
-	
+	var ancho;
+	var largo;
+	var metrosAlambreAncho;
+	var metrosAlambreLargo;
+	var metrosAlambreTotal
+
+	ancho=document.getElementById('ancho').value;
+	largo=document.getElementById('largo').value; //En el documento HTML le borré donde decía readonly en la parte del ID para poder cargar el largo en la pantalla.
+
+	metrosAlambreAncho = ancho*12;
+	metrosAlambreLargo=largo*12;
+
+	metrosAlambreLargo=parseFloat(metrosAlambreLargo);
+	metrosAlambreAncho=parseFloat(metrosAlambreAncho);
+
+	metrosAlambreTotal=metrosAlambreLargo+metrosAlambreAncho;
+
+	alert("Se necesitarán "+metrosAlambreTotal+" metros de alambre.");
+
 }
+
 
--- 9-Parciales/recuperatorio2/4-if.js
+++ 9-Parciales/recuperatorio2/4-if.js
@@ -1,7 +1,35 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
+	var numeroUno;
+	var numeroDos;
+	var multiplicar;
+	var resta;
+
+	numeroUno=prompt("Ingrese un numero:");
+	numeroDos=prompt("Ingrese otro numero:");
+
+	numeroUno=parseFloat(numeroUno);
+	numeroDos=parseFloat(numeroDos);
+
+//Recordar poner los parseFloat antes del primer if
 	
+
+	if(numeroUno==numeroDos)
+	{
+		multiplicar=numeroUno*numeroDos;
+		document.write(multiplicar);
+	}
+else
+	if(numeroUno>numeroDos)
+	{
+		resta=numeroUno-numeroDos;
+		document.write(resta);
+	}
+else
+	{
+		suma=numeroUno+numeroDos;
+		document.write(suma);
+	}
 }
 
