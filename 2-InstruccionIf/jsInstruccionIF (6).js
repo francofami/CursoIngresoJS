14c6b8c9040dc8b20cdacf864f16c23aec75892a
francofami <31368175+francofami@users.noreply.github.com>
2018-02-14 09:00:01 -0300

    6 corrección
    Le quité un if que no era necesario.

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -6,19 +6,14 @@ var edad;
 edad=document.getElementById('edad').value;
 
 if(edad>=18)
-{
-	alert("Es mayor de edad");
-}
-else
-	if(edad>=13 && edad<=17)
+	{
+		alert("Es mayor de edad");
+	}
+else if(edad>=13 && edad<=17)
 	{
 		alert("Es adolescente");
 	}
 else
-	if(edad<=12)
-	{
 		alert("Es niño/a");
-	}
-
 
 }//FIN DE LA FUNCIÓN
