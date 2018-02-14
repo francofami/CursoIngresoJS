839c47addeaa0ea62e8019eb201384eb8fd4627b
francofami <31368175+francofami@users.noreply.github.com>
2018-02-14 11:51:20 -0300

    4

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,8 +5,22 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
-
-
+switch(mesDelAño)
+{
+	case "Febrero":
+		alert("Tiene 28 días.");
+		break;
+	case "Enero":
+	case "Marzo":
+	case "Mayo":
+	case "Julio":
+	case "Agosto":
+	case "Octubre":
+	case "Diciembre":
+		alert("Tiene 31 días");
+		break;
+	default:
+		alert("Tiene 30 días");
+}
 
 }//FIN DE LA FUNCIÓN
