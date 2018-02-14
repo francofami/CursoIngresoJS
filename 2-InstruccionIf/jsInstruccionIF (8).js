58167b8d5ec7e1cb69e7be9c8328538def0acaf3
francofami <31368175+francofami@users.noreply.github.com>
2018-02-14 09:46:12 -0300

    8
    Funciones console.log() y console.info() agregadas y explicadas.

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -5,14 +5,25 @@ function Mostrar()
 	var edad;
 	var estadoCivil;
 
+	//console.log("Hola"); Esto es para mostrar un mensaje por consola cuando apreto el botón en la pagina html
+
+	//console.info("La variable es", estadoCivil); Esto nos muestra información sobre la variable (va a servir mas adelante para array)
+
 	edad=document.getElementById('edad').value;
 	estadoCivil=document.getElementById('estadoCivil').value;
 
+	if(edad<18 && estadoCivil!="Soltero")
+		{
+			console.log("NO HACER NADA");
+		}
+
+/* Como me pide que no haga nada, muestro un mensaje en la consola que diga
+no hacer nada */
 
 	if(edad>=18 && estadoCivil=="Soltero")
-	{
-		alert("Es soltero y no es menor");
-	}
+		{
+			alert("Es soltero y no es menor");
+		}
 
 
 }//FIN DE LA FUNCIÓN
