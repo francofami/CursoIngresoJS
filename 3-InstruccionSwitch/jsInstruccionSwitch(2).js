ded230327763dae0a37586049e2f0bb4eb11f8d8
francofami <31368175+francofami@users.noreply.github.com>
2018-02-14 11:45:17 -0300

    2

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,7 +3,28 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch(mesDelAño)
+{
+	case "Enero":
+	case "Febrero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+		alert("Falta para el invierno.");
+		break;
+	case "Junio":
+	case "Julio":
+	case "Agosto":
+		alert("Abrigate que hace frio.");
+		break;
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+		alert( "Ya pasamos el frio, ahora calor!!!.");
+	//Despues de la ultima linea de codigo dentro de un switch no hace falta poner un break.
+}
+
 
 
 
