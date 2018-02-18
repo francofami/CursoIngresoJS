e7a3567df880133a0e9ee9ae2e1c401e92199486
francofami <31368175+francofami@users.noreply.github.com>
2018-02-17 23:48:11 -0300

    5

--- 9-Parciales/parcial/cinco.js
+++ 9-Parciales/parcial/cinco.js
@@ -1,4 +1,19 @@
 function Mostrar()
 {
+	var diaSemana;
 
+	diaSemana=prompt("Ingrese día de la semana:");
+
+	switch(diaSemana)
+	{
+		case "Sábado":
+		case "Domingo":
+
+		alert("es fin de semana");
+		break;
+
+		default:
+
+		alert("a trabajar !!!");
+	}
 }
