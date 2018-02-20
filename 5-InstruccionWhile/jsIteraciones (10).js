b4e674811166cf8283e23962b9d9682e806acb8b
francofami <31368175+francofami@users.noreply.github.com>
2018-02-20 12:22:42 -0300

    10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -2,17 +2,52 @@ function Mostrar()
 {
 
 	var contador=0;
+	var numero;
+	var cantidadPositivos;
+	var cantidadNegativos;
 	//declarar contadores y variables 
 	
 	var respuesta="si";
+	var negativos;
+	var positivos;
+	var ceros=0;
+	var pares=0;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferencia;
 
 	while(respuesta!="no")
 	{
-		
-	
+		numero= prompt("Ingrese un número.");
+
+		respuesta=prompt("Responda no para dejar de ingresar mas numeros");
+
+		if(numero<0)
+		{
+			negativos+=numero;
+			cantidadNegativos++;
+		}
+		else if(numero>0)
+		{
+			positivos+=numero;
+			cantidadPositivos++;
+		}
+		else
+		{
+			ceros++;
+		}
+		if(numero%2==0)
+		{
+			pares++;
+		}
 	}
 
+	promedioPositivos=positivos/cantidadPositivos;
+	promedioNegativos=negativos/cantidadNegativos;
 
+	diferencia=positivos-negativos;
+
+	document.write("")
 
 
 }//FIN DE LA FUNCIÓN
