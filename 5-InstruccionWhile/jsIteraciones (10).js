1ec3d6c390bd41848ae4e844c3166e69914c902e
francofami <31368175+francofami@users.noreply.github.com>
2018-02-21 00:08:59 -0300

    10
    ejercicio terminado

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,15 +1,12 @@
 function Mostrar()
 {
 
-	var contador=0;
 	var numero;
-	var cantidadPositivos;
-	var cantidadNegativos;
-	//declarar contadores y variables 
-	
+	var cantidadPositivos=0;
+	var cantidadNegativos=0;
 	var respuesta="si";
-	var negativos;
-	var positivos;
+	var negativos=0;
+	var positivos=0;
 	var ceros=0;
 	var pares=0;
 	var promedioPositivos;
@@ -20,16 +17,17 @@ function Mostrar()
 	{
 		numero= prompt("Ingrese un número.");
 
-		respuesta=prompt("Responda no para dejar de ingresar mas numeros");
 
 		if(numero<0)
 		{
+			numero=parseInt(numero);
 			negativos+=numero;
 			cantidadNegativos++;
 		}
 		else if(numero>0)
 		{
-			positivos+=numero;
+			numero=parseInt(numero);
+			positivos= positivos+numero;
 			cantidadPositivos++;
 		}
 		else
@@ -40,14 +38,21 @@ function Mostrar()
 		{
 			pares++;
 		}
+
+		respuesta=prompt("Responda no para dejar de ingresar mas numeros");
 	}
+
+
 
 	promedioPositivos=positivos/cantidadPositivos;
 	promedioNegativos=negativos/cantidadNegativos;
 
 	diferencia=positivos-negativos;
 
-	document.write("")
+	document.write("La suma de los negativos es de: "+negativos+"<br>"+"La suma de los positivos es de: "+positivos+"<br>"+
+		"Hay "+cantidadPositivos+" numeros positivos. <br>"+"Hay "+cantidadNegativos+" numeros negativos. <br>"+"Hay "+ceros+" ceros. <br>"
+		+"Hay "+pares+" numeros pares. <br>"+"El promedio de los numeros positivos es: "+promedioPositivos+"<br>"+
+		"El promedio de los numeros negativos es: "+promedioNegativos+"<br>"+"La diferencia entre positivos y negativos es: "+diferencia);
 
 
 }//FIN DE LA FUNCIÓN
