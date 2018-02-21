8c07bcfe915f11bbfac383f63745e0dcb87b1a68
francofami <31368175+francofami@users.noreply.github.com>
2018-02-21 11:13:58 -0300

    8

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,25 @@
 function Mostrar()
 {
+	var numero;
+	var cantidadDivisores=0;
 
+	numero=prompt("Ingrese un número: ");
 
+	for(i=1;i<=numero;i++)
+	{
+		if(numero%i==0)
+		{
+			cantidadDivisores++;
+		}
+	}
 
+	if(cantidadDivisores==2 || numero==1 || numero==-1)
+	{
+		alert("El numero es primo.");
+	}
+	else 
+	{
+		alert("El numero no es primo.");
+	}
 
 }//FIN DE LA FUNCIÓN
