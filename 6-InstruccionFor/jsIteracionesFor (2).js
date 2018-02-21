02903a8bbb8e94dc73b87d4b26969511fc6507fe
francofami <31368175+francofami@users.noreply.github.com>
2018-02-21 11:27:54 -0300

    2
    agregue comentado como funcionan break y continue dentro de if

--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,9 +1,28 @@
 function Mostrar()
 {
-	var i;
 
-	for(i=10; i>0; i--)
+	for(i=10;i>0;i--)
 	{
 		document.write(i+"<br>");
 	}
+
+	/*for(i=0; i<10; i++)
+	{
+		document.write("<br>"+i);
+
+		if(i==10)
+		{
+			break;
+		}
+
+		if(i==5)
+		{
+			continue;
+		}
+
+		document.write("Despues de continue");
+
+	}
+
+	document.write("Fin del for");*/
 }
