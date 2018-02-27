29e9785b407c9afd9d0818f42e366f203dad530f
francofami <31368175+francofami@users.noreply.github.com>
2018-02-27 12:23:01 -0300

    7

--- 9-Parciales/recuperatorio/siete.js
+++ 9-Parciales/recuperatorio/siete.js
@@ -1,4 +1,27 @@
 function Mostrar()
 {
+	var edad;
+	var sexo;
+	var promedio;
+	var contadorVarones;
+	var respuesta="si";
 
+	while(respuesta!="no")
+	{
+		edad=prompt("Ingrese edad: ");
+		
+		while(edad<0 || edad>100 || edad=="" || isNaN(edad))
+		{
+			edad=prompt("¡Error! Ingrese edad (entre 0 y 100): ");
+		}
+
+		edad=prompt("Ingrese sexo (f o m): ");
+		
+		while(sexo!="f" && sexo !="m")
+		{
+			edad=prompt("¡Error! Ingrese sexo (f o m): ");
+		}
+
+		
+	}
 }
