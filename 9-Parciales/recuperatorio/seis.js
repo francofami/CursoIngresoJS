c1b2ed981858841e4d83760fa72b0f4bb8b6a9d4
francofami <31368175+francofami@users.noreply.github.com>
2018-02-27 12:17:27 -0300

    6

--- 9-Parciales/recuperatorio/seis.js
+++ 9-Parciales/recuperatorio/seis.js
@@ -1,4 +1,47 @@
 function Mostrar()
 {
+	var peso;
+	var contador=1;
+	var masPesado;
+	var menosPesado;
+	var contenedorMasPesado;
+	var contenedorMenosPesado;
+
+	while(contador<=5)
+	{
+		peso=prompt("Ingrese peso: ");
+	
+		while(peso<=0)
+		{
+			peso=prompt("¡Error! Ingrese peso (mayor a 0): ");
+		}
+
+		if(contador==1)
+		{
+			masPesado=peso;
+			menosPesado=peso;
+			contenedorMasPesado=contador;
+			contenedorMenosPesado=contador;
+		}
+
+		else if(masPesado<peso)
+		{
+			masPesado=peso;
+			contenedorMasPesado=contador;
+		}
+
+		if(menosPesado>peso)
+		{
+			menosPesado=peso;
+			contenedorMenosPesado=contador;
+		}
+
+		contador++;
+
+	}
+
+	alert("El mas pesado fue el contenedor: "+contenedorMasPesado);
+	alert("El menos pesado fue el contenedor: "+contenedorMenosPesado);
+
 
 }
